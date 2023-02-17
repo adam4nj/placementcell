bf8bff4ba2f6b5b9ca7c0dc108a779b726fc33e5
adamj <adamjamesnellissery@gmail.com>
2023-02-17 22:07:49 +0530

    created model Job_apply

--- jobs/models.py
+++ jobs/models.py
@@ -1,19 +1,38 @@
 from django.db import models
 from django.utils import timezone
-from users.models import CustomUser,Company
+from users.models import Student, Company
 
 # Create your models here.
 
 
 class Job(models.Model):
     title = models.CharField(max_length=100)
-    desc = models.TextField()
+    location = models.CharField(max_length=20)
     date_posted = models.DateTimeField(default=timezone.now)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    last_date = models.DateField()
+    company = models.ForeignKey(Company,on_delete=models.CASCADE)
     salary = models.PositiveIntegerField(max_length=10)
-
+    desc = models.TextField()
 
     def __str__(self):
         return self.title
+    
+class Job_apply(models.Model):
+    job = models.ForeignKey(Job,on_delete=models.CASCADE)
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    OPTIONS = (
+        ('0', 'Accepted'),
+        ('1', 'Pending'),
+        ('2', 'Rejected'),
+    )
+    status = models.CharField(max_length=1,choices=OPTIONS,default='1')
+    
+    def __str__(self):
+        return self.job
+
+
+
+    
+
 
 
