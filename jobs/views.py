7616e03c44dffcc2f72b221f5c8174f583ccc708
adamj <adamjamesnellissery@gmail.com>
2023-02-17 22:42:46 +0530

    added urls and views

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Job
+
+def joblist(request):
+    context = {
+        'jobs': Job.objects.all()
+    }
+    return render(request, 'jobs/list.html', context)
