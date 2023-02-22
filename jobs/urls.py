a21068440f2ff477c2671338356c00b191f474ad
adamj <adamjamesnellissery@gmail.com>
2023-02-22 07:31:53 +0530

    navbar items

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.home, name='home'),
+    path('about/',views.about,name='about'),
     path('jobs/', views.joblist, name='jobs'),
+    path('contact/',views.contact,name='contact'),
+
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -4,8 +4,17 @@ from django.shortcuts import render
 
 from .models import Job
 
+def home(request):
+    return render(request,'jobs/home.html')
+
+def about(request):
+    return render(request,'jobs/about.html')
+
 def joblist(request):
     context = {
         'jobs': Job.objects.all()
     }
     return render(request, 'jobs/jobs.html', context)
+
+def contact(request):
+    return render(request,'jobs/contact.html')
